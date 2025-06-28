b0da08374c9762f664cfe3d8557527ea9c030b9e
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2025-06-28 11:01:54 +0530

    added the todo backend app

--- todo-app/todo-backend/src/main/java/com/example/todobackend/TodoBackendApplication.java
+++ todo-app/todo-backend/src/main/java/com/example/todobackend/TodoBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.todo_backend;
+package com.example.todobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
